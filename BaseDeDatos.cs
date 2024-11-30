66c9402003ffe1e2e1c115a04e3652ea62e954d7
AnthonnyM31 <anthonnymosquera1492@gmail.com>
2024-11-30 18:04:54 -0500

    modificamos levemente la clase Nota para poder usarla para la base de datos creando un id y declarando como un PrimaryKey y un AutoIncrement, a su vez creamos el crud de la base de datos

---
+++ BaseDeDatos.cs
@@ -0,0 +1,33 @@
+﻿using SQLite;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using static MosqueraAnthonny_TareaApuntes.NuevaNotaPage;
+
+namespace MosqueraAnthonny_TareaApuntes;
+
+public class BaseDeDatos
+{
+    private readonly SQLiteAsyncConnection _database;
+
+    public BaseDeDatos(string dbPath)
+    {
+        _database = new SQLiteAsyncConnection(dbPath);
+        _database.CreateTableAsync<Nota>().Wait();
+    }
+
+    public Task<List<Nota>> ObtenerNotasAsync()
+    {
+        return _database.Table<Nota>().ToListAsync();
+    }
+
+    public Task<int> GuardarNotaAsync(Nota nota)
+    {
+        return nota.Id != 0 ? _database.UpdateAsync(nota) : _database.InsertAsync(nota);
+    }
+
+    public Task<int> EliminarNotaAsync(Nota nota)
+    {
+        return _database.DeleteAsync(nota);
+    }
+}
--- NuevaNotaPage.xaml.cs
+++ NuevaNotaPage.xaml.cs
@@ -1,3 +1,4 @@
+using SQLite;
 namespace MosqueraAnthonny_TareaApuntes;
 public partial class NuevaNotaPage : ContentPage
 {
@@ -11,6 +12,9 @@ public partial class NuevaNotaPage : ContentPage
 
     public class Nota
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
         //si no están marcadas como opcionales o requeridas no se por que pero no funciona asi que vamos a permitir valores null
         public  string? Titulo { get; set; }
         public  string? Contenido { get; set; }
